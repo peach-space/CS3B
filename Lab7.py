4897bd3d0a05f703765c085af51ad1dbf7832972
Frank <Frank@jy.com>
2026-02-22 17:44:39 -0800

    lab 7

--- Lab7.py
+++ Lab7.py
@@ -9,7 +9,7 @@ orders = [[34587, "Learning Python, Mark Lutz", 4, 40.95],
           [88112, "Einführung in Python3, Bernd Klein", 3, 24.99]]
 
 
-order_prices = list(map(lambda x: (x[0], x[2] * x[3] + 10 if x[2] * x[3] < 100 else x[2] * x[3]), orders))
+order_prices = list(map(lambda x: (x[0], round(x[2] * x[3] + 10 if x[2] * x[3] < 100 else x[2] * x[3], 2)), orders))
 print(order_prices)
 
 # order_prices = []
