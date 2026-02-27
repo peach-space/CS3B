7abfef94b1bc636542819d45c00269fa6f1887eb
Frank <Frank@jy.com>
2026-02-27 01:30:11 -0800

    lab 8

---
+++ Lab8.py
@@ -0,0 +1,22 @@
+#################################################
+# Lab 8
+# Student Name: Cen Li
+#################################################
+
+def rotate_reversal(nums, k):
+    n = len(nums)
+    k = k % n
+
+    def reverse(start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    reverse(0, n - 1)
+    reverse(0, k - 1)
+    reverse(k, n - 1)
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+rotate_reversal(nums, 3)
+print(nums)
